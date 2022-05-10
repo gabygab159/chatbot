cc85d92586f538c5fcd7cfedb2b4150425ab63ff
gaby <gabriel.jchen@gmail.com>
2022-05-09 23:14:28 -0400

    add types and handlesubmit

--- src/components/prompt/Prompt.tsx
+++ src/components/prompt/Prompt.tsx
@@ -1,17 +1,29 @@
 import { Button, Input } from "@chakra-ui/react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function Prompt() {
   const [prompt, setPrompt] = useState("");
-  const handleChange = (e: any) => {
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPrompt(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLButtonElement>): void => {
+    e.preventDefault();
+    console.log(prompt);
   };
 
   return (
     <div>
       <label htmlFor="prompt">Prompt</label>
-      <Input placeholder="Ask chatbot something..." value={prompt} onChange={handleChange}></Input>
-      <Button>Submit</Button>
+      <Input
+        placeholder="Ask chatbot something..."
+        value={prompt}
+        onChange={handleChange}
+      ></Input>
+      <Button type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
     </div>
   );
 }
